bf5622724b08e4cffb51d621f66823b16a80c0c9
Andrzej Kupczyk <kontakt@andrzejkupczyk.pl>
2022-01-23 10:42:50 +0100

    Remove vue instance from window object

--- files/todolists.js
+++ files/todolists.js
@@ -1,88 +1,84 @@
-(() => {
-
-  window.ToDoList = new Vue({
-    el: '#ToDoLists',
-    props: ['currentTasks', 'isReadonly', 'translations'],
-    data: {
-      newTask: {
-        bug_id: 0,
-        description: '',
-        descriptionHtml: '',
-      },
-      tasks: [],
-      lang: undefined
+new Vue({
+  el: '#ToDoLists',
+  props: ['currentTasks', 'isReadonly', 'translations'],
+  data: {
+    newTask: {
+      bug_id: 0,
+      description: '',
+      descriptionHtml: '',
     },
-    computed: {
-      action() {
-        return this.$el.getElementsByTagName('FORM')[0].action;
-      },
-      finishedTasks() {
-        return this.tasks.filter((task) => task.finished);
-      },
-      counter() {
-        const tasksLeft = this.tasks.length - this.finishedTasks.length;
-
-        return [tasksLeft, this.tasks.length].join('/');
-      },
+    tasks: [],
+    lang: undefined
+  },
+  computed: {
+    action() {
+      return this.$el.getElementsByTagName('FORM')[0].action;
     },
-    methods: {
-      insertTask() {
-        if (!this.validateDescription(this.newTask.description)) {
-          return;
-        }
-
-        axios.post(this.action, {task: this.newTask}).then((response) => {
-          this.tasks.push(...response.data);
-          this.newTask.description = this.newTask.descriptionHtml = '';
-        });
-      },
-      updateTask(task) {
-        return axios.post(this.action, {task: task}, {
-          headers: {'x-http-method-override': 'put'}
-        });
-      },
-      deleteTask(task) {
-        if (!task.finished && !confirm(this.lang?.confirmDeletion)) {
-          return;
-        }
-
-        axios.post(this.action, {task: {id: task.id}}, {
-          headers: {'x-http-method-override': 'delete'}
-        }).then(() => {
-          this.tasks.splice(this.tasks.indexOf(task), 1);
-        });
-      },
-      toggleFinished(task) {
-        task.finished = !!task.finished;
-
-        this.updateTask(task).then(
-          (response) => task = Object.assign(task, response.data),
-          () => task.finished = !task.finished
-        );
-      },
-      changeDescription(task) {
-        const origDesc = task.description;
-        const newDesc = prompt(this.lang?.enterNewDescription, task.description);
-
-        if (!this.validateDescription(newDesc) || newDesc === origDesc) {
-          return;
-        }
-
-        task.description = newDesc;
-
-        this.updateTask(task).then(
-          (response) => task.descriptionHtml = response.data.descriptionHtml,
-          () => task.description = origDesc
-        );
-      },
-      validateDescription(description) {
-        return (description ? description.length : 0) > 0;
-      },
+    finishedTasks() {
+      return this.tasks.filter((task) => task.finished);
     },
-    created() {
-      this.tasks = JSON.parse(this?.currentTasks);
-      this.lang = JSON.parse(this?.translations);
-    },
-  });
+    counter() {
+      const tasksLeft = this.tasks.length - this.finishedTasks.length;
 
-})();
+      return [tasksLeft, this.tasks.length].join('/');
+    },
+  },
+  methods: {
+    insertTask() {
+      if (!this.validateDescription(this.newTask.description)) {
+        return;
+      }
+
+      axios.post(this.action, {task: this.newTask}).then((response) => {
+        this.tasks.push(...response.data);
+        this.newTask.description = this.newTask.descriptionHtml = '';
+      });
+    },
+    updateTask(task) {
+      return axios.post(this.action, {task: task}, {
+        headers: {'x-http-method-override': 'put'}
+      });
+    },
+    deleteTask(task) {
+      if (!task.finished && !confirm(this.lang?.confirmDeletion)) {
+        return;
+      }
+
+      axios.post(this.action, {task: {id: task.id}}, {
+        headers: {'x-http-method-override': 'delete'}
+      }).then(() => {
+        this.tasks.splice(this.tasks.indexOf(task), 1);
+      });
+    },
+    toggleFinished(task) {
+      task.finished = !!task.finished;
+
+      this.updateTask(task).then(
+        (response) => task = Object.assign(task, response.data),
+        () => task.finished = !task.finished
+      );
+    },
+    changeDescription(task) {
+      const origDesc = task.description;
+      const newDesc = prompt(this.lang?.enterNewDescription, task.description);
+
+      if (!this.validateDescription(newDesc) || newDesc === origDesc) {
+        return;
+      }
+
+      task.description = newDesc;
+
+      this.updateTask(task).then(
+        (response) => task.descriptionHtml = response.data.descriptionHtml,
+        () => task.description = origDesc
+      );
+    },
+    validateDescription(description) {
+      return (description ? description.length : 0) > 0;
+    },
+  },
+  created() {
+    this.tasks = JSON.parse(this?.currentTasks);
+    this.lang = JSON.parse(this?.translations);
+  },
+});
