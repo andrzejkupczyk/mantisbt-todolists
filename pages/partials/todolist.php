7a5aeee4d2b94b3012e0c43c076f19f5ed5e95bc
Andrzej Kupczyk <kontakt@andrzejkupczyk.pl>
2022-07-16 17:32:58 +0200

    Improve HTML markup and CSS styling

--- pages/partials/todolist.php
+++ pages/partials/todolist.php
@@ -2,69 +2,45 @@
 if (!access_has_project_level(plugin_config_get('view_threshold'))) {
     return;
 }
+$canManage = access_has_project_level(plugin_config_get('manage_threshold'));
 ?>
-<tr id="<?= plugin_get_current() ?>"
-    :is-readonly="<?= !access_has_project_level(plugin_config_get('manage_threshold')) ? 'true' : 'false' ?>"
-    current-tasks="<?= string_attribute(json_encode($tasks ?? [])) ?>"
-    translations="<?= string_attribute(json_encode([
-        'enterNewDescription' => plugin_lang_get('enter_new_description'),
-        'confirmDeletion' => plugin_lang_get('confirm_deletion'),
-    ])) ?>"
->
+<tr class="<?= plugin_get_current() ?>">
     <td class="category">
         <?= plugin_lang_get('things_to_do') ?>
-        <span class="<?= plugin_get_current() ?>-counter" v-cloak>
-      {{ counter }}
-    </span>
     </td>
     <td colspan="5">
-        <form
-                class="<?= plugin_get_current() ?>-form form-inline"
-                action="<?= plugin_page('ajax_page') ?>"
-                method="post"
-                @submit.prevent
-        >
-            <input v-model="newTask.bug_id" type="hidden" value="<?= $bugId ?>">
+        <?php if ($canManage): ?>
+        <form>
             <textarea
-                    @keydown.enter.prevent="insertTask"
-                    v-model="newTask.description"
-                    v-if="!isReadonly"
-                    rows="1"
-                    class="<?= plugin_get_current() ?>-add-new input-sm"
-                    placeholder="<?= plugin_lang_get('add_new_task') ?>"
+                rows="1"
+                class="input-sm"
+                placeholder="<?= plugin_lang_get('add_new_task') ?>"
             ></textarea>
-            <button class="btn btn-primary btn-sm btn-white btn-round" @click="insertTask" v-if="!isReadonly">
+            <button class="btn btn-primary btn-sm btn-white btn-round">
                 <?= plugin_lang_get('add') ?>
             </button>
-            <ul v-if="tasks.length" v-cloak>
-                <li v-for="task in tasks" track-by="id">
-                    <label :class="{finished: task.finished}">
-                        <input
-                                @change="toggleFinished(task)"
-                                v-model="task.finished"
-                                :disabled="isReadonly"
-                                type="checkbox"
-                        >
-                        <span v-html="task.descriptionHtml"></span>
-                    </label>
-                    <span class="<?= plugin_get_current() ?>-task-actions ?>">
-                        <a
-                                @click="changeDescription(task, $event)"
-                                v-if="!isReadonly && !task.finished"
-                                title="<?= plugin_lang_get('edit_task') ?>"
-                        >
+        </form>
+        <?php endif ?>
+        <?php if(!empty($tasks)): ?>
+        <ul class="<?= plugin_get_current() ?>-items <?= $canManage ? 'manageable' : '' ?>">
+            <?php foreach ($tasks as $task): ?>
+                <li class="<?= plugin_get_current() ?>-item <?= $task['finished'] ? 'finished' : 'unfinished'?>">
+                    <span class="description">
+                        <?= $task['description'] ?>
+                    </span>
+                    <?php if ($canManage): ?>
+                    <span class="actions">
+                        <a class="edit" title="<?= plugin_lang_get('edit_task') ?>">
                             <i class="fa fa-pencil"></i>
                         </a>
-                    <a
-                            @click="deleteTask(task, $event)"
-                            v-if="!isReadonly"
-                            title="<?= plugin_lang_get('delete_task') ?>"
-                    >
-                        <i class="fa fa-trash"></i>
-                    </a>
+                        <a title="<?= plugin_lang_get('delete_task') ?>">
+                            <i class="fa fa-trash"></i>
+                        </a>
                     </span>
+                    <?php endif ?>
                 </li>
-            </ul>
-        </form>
+            <?php endforeach ?>
+        </ul>
+        <?php endif ?>
     </td>
 </tr>
